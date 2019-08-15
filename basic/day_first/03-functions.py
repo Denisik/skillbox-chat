95297882e5c03f81070597ed1eb8e1098643fdd5
Denis <denis.kostyanets@gmail.com>
2019-08-15 11:13:20 -0700

    3 Commit

--- basic/day_first/03-functions.py
+++ basic/day_first/03-functions.py
@@ -5,3 +5,24 @@
 #
 #  Работа с функциями, аргументы и возвращаемое значение
 #
+# def my_print(message: str):
+#     print("My print: " + message)
+# my_print("some 1")
+# my_print("some 2")
+#
+# def show_counter(step, max_step):
+#     while step <= max_step:
+#         print(step)
+#         step += 1
+#
+#
+#
+# show_counter(5, 15)
+
+
+def sum_of_two_numbers(a, b):
+    print(a + b)
+    return a + b
+
+answer = sum_of_two_numbers(20, 20)
+print(answer)
--- basic/day_first/04-classes.py
+++ basic/day_first/04-classes.py
@@ -5,3 +5,16 @@
 #
 #  Основы ООП, класс, объект, метод и атрибут
 #
+user = ['admin', '123123', 43]
+user_dict = {'login': 'admin', 'pass': '123123', 'age': 43}
+class User:
+    login: str
+    age: int
+    password: str
+user1 = User()
+user1.age = 50
+
+
+user2 = User()
+user2.age = 100
+
