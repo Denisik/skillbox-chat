4de2f83b2e94598ca47b7de7205c4d242a76dd58
Artem Manchenkov <artyom@manchenkoff.me>
2019-08-15 13:27:42 +0300

    code examples added

--- examples/01-syntax.py
+++ examples/01-syntax.py
@@ -6,3 +6,18 @@
 #  Базовые операции языка
 #
 
+# Объявление переменных
+a = 10
+b = 15 + 14
+c = a * 2
+d = b + c
+
+# Вывод данных
+print(10)
+print(a)
+print("Hello world")
+print("Hello ", "YourName")
+
+# Ввод данных
+your_name = input("Enter your first name: ")
+your_age = int(input("Enter your age: "))  # преобразовали в int число
--- examples/10-wxpython.py
+++
@@ -1,7 +0,0 @@
-#  Created by Artem Manchenkov
-#  artyom@manchenkoff.me
-#
-#  Copyright © 2019
-#
-#  Примеры использования библиотеки wxPython
-#
