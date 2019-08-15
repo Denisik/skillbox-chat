1cc163580a356750ea36054a1bb7922b4070b504
Denis <denis.kostyanets@gmail.com>
2019-08-15 10:11:22 -0700

    1 Commit

--- basic/day_first/01-intro.py
+++ basic/day_first/01-intro.py
@@ -6,3 +6,26 @@
 #  Примеры базового синтаксиса и работа с типами данных
 #  Числа, строки, списки, булево значение
 #
+a = 1 + 2
+a = a + a
+
+integer_number = 10
+float_number = 10.4
+
+string_value = "Text"
+string_value = string_value * 2 # TextText
+string_value = string_value + string_value
+
+string_value.lower()
+
+numbers = [1, 2, 3, "Text", 10.5]
+
+
+
+a = True
+b = False
+
+result = 10 < 5
+
+
+
