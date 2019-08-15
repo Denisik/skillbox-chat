88fcf5ce95ec8d6e6e0ce80c97ec579e3db40f7d
Denis <denis.kostyanets@gmail.com>
2019-08-15 10:27:02 -0700

    2 Commit

--- basic/day_first/01-intro.py
+++ basic/day_first/01-intro.py
@@ -9,16 +9,16 @@
 a = 1 + 2
 a = a + a
 
-integer_number = 10
-float_number = 10.4
+integer_number = 10 #int
+float_number = 10.4 #float
 
-string_value = "Text"
+string_value = "Text" #str
 string_value = string_value * 2 # TextText
 string_value = string_value + string_value
 
 string_value.lower()
 
-numbers = [1, 2, 3, "Text", 10.5]
+numbers = [1, 2, 3, "Text", 10.5] #list
 
 
 
--- basic/day_first/02-expressions.py
+++ basic/day_first/02-expressions.py
@@ -5,3 +5,21 @@
 #
 #  Условные и циклические конструкции языка
 #
+age = int(input("Type your age: "))
+
+if age < 18:
+    print("Bye!")
+elif age == 18:
+    print()
+else:
+    print("Hello!")
+
+print(age)
+
+step = 1
+max_step = 200
+
+while step <= max_step:
+    print(step)
+    step += 1
+
